2b0ea8d2544e68d52010d5cf7bf3398e2dbf88a9
tomyMm <162061401+tomyMm@users.noreply.github.com>
2025-06-22 20:05:34 -0300

    Formulario Proyectos andando

---
+++ TP-Grupal-Programacion-1-master/Tp-Programacion I/Negocio/Estado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_Programacion_I.Negocio
+{
+    public class Estado
+    {
+        public int Codigo { get; set; }
+        public string EstadoProyecto { get; set; }
+    }
+}
